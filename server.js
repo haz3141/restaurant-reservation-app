c380d8649b41e032cd5cac5d0ac5dbc9ea6286e5
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-08-27 18:59:30 -0400

    initial code

---
+++ server.js
@@ -0,0 +1,20 @@
+// DEPENDENCIES
+    // IMPORT EXPRESS
+    // IMPORT FS
+    // INITIALIZE EXPRESS
+    // SET PORT
+
+// DATA
+    // CREATE TABLE
+        // ID, Name, Email, Phone
+    // CREATE RESTAURANT
+        // Reservations Array of Tables
+
+// ROUTES
+    // Home
+    // Make Reservations
+    // View Reservations
+    // POST Table
+    // GET Table
+
+// LISTEN
