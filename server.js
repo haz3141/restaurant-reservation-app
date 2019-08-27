d7134e8e50b46816d4739483a1d0ea4b78fb3f0a
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-08-27 19:37:56 -0400

    routes point to html files

--- server.js
+++ server.js
@@ -1,42 +1,53 @@
+const express = require('express');
+const fs = require('fs');
 
-    const express = require('express');
-    const fs = require('fs');
-    const app = express();
-    const PORT = 3000;
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 class Table {
-    constructor(id, name, email, phone) {
-        this.id = id;
-        this.name =name;
-        this.email = email;
-        this.phone = phone;
-    }
+	constructor(id, name, email, phone) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+	}
 }
-        
+
 class Restaurant {
-    constructor() {
-        this.reservations = [];
-        this.waitlist = [];
-    }
+	constructor() {
+		this.reservations = [];
+		this.waitlist = [];
+	}
 }
 
 // ROUTES
-    // Home
-    app.get('/', function(req, res) {
-        res.send('Home');
-    });
-    // Make Reservations
-    app.get('/reserve', function(req, res) {
-        res.send('Make Reservations');
-    });
-    // View Reservations
-    app.get('/tables', function(req, res) {
-        res.send('Tables');
-    });
-    // POST Table
-    // GET Table
+// Home
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+// Make Reservations
+app.get('/reserve', function(req, res) {
+	res.sendFile(path.join(__dirname, 'reserve.html'));
+});
+// View Reservations
+app.get('/tables', function(req, res) {
+	res.sendFile(path.join(__dirname, 'tables.html'));
+});
+// POST Table
+app.post('/api/tables', function(req, res) {
+    const newTable = req.body;
+    reservations.push(newTable);
+});
+
+// GET Table
+app.get('/api/tables', function(req, res) {
+	return res.json(reservations);
+});
 
 // LISTEN
 app.listen(PORT, function() {
-    console.log(`App listening on PORT ${PORT}`);
+	console.log(`App listening on PORT ${PORT}`);
 });
