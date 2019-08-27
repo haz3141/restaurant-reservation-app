de7c267bb0ee043f268a99a39fd55726fd4e498c
haz3141 <38766403+haz3141@users.noreply.github.com>
2019-08-27 19:18:50 -0400

    basic server setup

--- server.js
+++ server.js
@@ -1,20 +1,42 @@
-// DEPENDENCIES
-    // IMPORT EXPRESS
-    // IMPORT FS
-    // INITIALIZE EXPRESS
-    // SET PORT
 
-// DATA
-    // CREATE TABLE
-        // ID, Name, Email, Phone
-    // CREATE RESTAURANT
-        // Reservations Array of Tables
+    const express = require('express');
+    const fs = require('fs');
+    const app = express();
+    const PORT = 3000;
+
+class Table {
+    constructor(id, name, email, phone) {
+        this.id = id;
+        this.name =name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
+        
+class Restaurant {
+    constructor() {
+        this.reservations = [];
+        this.waitlist = [];
+    }
+}
 
 // ROUTES
     // Home
+    app.get('/', function(req, res) {
+        res.send('Home');
+    });
     // Make Reservations
+    app.get('/reserve', function(req, res) {
+        res.send('Make Reservations');
+    });
     // View Reservations
+    app.get('/tables', function(req, res) {
+        res.send('Tables');
+    });
     // POST Table
     // GET Table
 
 // LISTEN
+app.listen(PORT, function() {
+    console.log(`App listening on PORT ${PORT}`);
+});
